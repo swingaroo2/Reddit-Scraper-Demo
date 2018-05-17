a73b108fbd89c62b5789499ce6fa6dd7a83694cf
Zach Lockett-Streiff <zachls17@gmail.com>
2018-05-16 21:30:20 -0500

    Initial Commit

---
+++ Reddit Scraper Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reddit Scraper Demo
+//
+//  Created by Zach Lockett-Streiff on 5/16/18.
+//  Copyright © 2018 Swingaroo2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
